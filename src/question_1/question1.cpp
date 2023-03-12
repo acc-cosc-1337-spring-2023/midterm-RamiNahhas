26e052d32395be4463a90c755cd0cd05f273baf3
RamiNahhas <123427547+RamiNahhas@users.noreply.github.com>
2023-03-12 10:11:53 +0000

    mid term

--- src/question_1/question1.cpp
+++ src/question_1/question1.cpp
@@ -1,6 +1,30 @@
+#include <iostream>
 #include "question1.h"
+
+using std::cout; using std::cin; using std:: string;
+
+using std:: to_string;
 
 bool test_config()
 {
     return true;
+}
+
+
+
+  string get_fib_sequence(int num) 
+  {
+    string sequence = "0 1 ";
+
+    int num1 = 0, num2 = 1, next_term = 1;
+
+    while (next_term <= num)
+     {
+        sequence += to_string(next_term) + " ";
+        num1 = num2;
+        num2 = next_term;
+        next_term = num1 + num2;
+    }
+
+    return sequence;
 }
